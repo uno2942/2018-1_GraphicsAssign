519950c8fbdb4dadb834155e851791c626994373
psb2942 <psb2942@gmail.com>
2019-03-11 17:20:38 +0900

    32비트도 되게 했습니다.

--- GraphicsAssign1/GraphicsAssign1/source.cpp
+++ GraphicsAssign1/GraphicsAssign1/source.cpp
@@ -1,19 +1,22 @@
-#include <iostream>
-#include <glew.h>
-#include <freeglut.h>
-using namespace std;
-void renderScene(void) {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glFlush();
+#include <glut.h> /* this includes the others */
+
+void myReshape(int w, int h) {
+	glLoadIdentity();
+	glViewport(0, 0, w, h);
+	gluOrtho2D(0.0, 100.0, 0.0, 100.0);
 }
-void main(int argc, char **argv) {
+void display(void) {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0.0, 1.0, 1.0);
+	glRectf(10.0, 10.0, 90.0, 90.0);
+	glutSwapBuffers();
+}
+void main(int argc, char **argv)
+{
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
-	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(320, 320);
-	glutCreateWindow("Hello OpenGL");
-	glutDisplayFunc(renderScene);
-	glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
-	glewInit();
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutCreateWindow((const char*)"simple");
+	glutReshapeFunc(myReshape);
+	glutDisplayFunc(display);
 	glutMainLoop();
 }
