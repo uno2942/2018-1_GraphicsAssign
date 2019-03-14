457e44b55ff2f763422598bec30db0e09b27158d
psb2942 <psb2942@gmail.com>
2019-03-14 19:36:12 +0900

    숫자에 따라 camMode를 바꾸는 것을 짰습니다.

--- GraphicsAssign1/GraphicsAssign1/main.cpp
+++ GraphicsAssign1/GraphicsAssign1/main.cpp
@@ -7,6 +7,7 @@ void head() {
 	int timeSinceStart = glutGet(GLUT_ELAPSED_TIME);
 	GameManager::getInstance().OneFramePipeline();
 }
+
 void idle2(int value) {
 	int timeSinceStart = glutGet(GLUT_ELAPSED_TIME);
 	cout << "Player: " << GameManager::getInstance().playerBox.position << '\t' << "Ball: " << GameManager::getInstance().ball.position << endl;
@@ -17,6 +18,13 @@ void idle2(int value) {
 void idlefunction(int value) {
 	head();
 	glutTimerFunc(3, idlefunction, 0);
+}
+
+void myKeyboard(unsigned char c, int x, int y) {
+	switch (c) {
+	case '1': camMode = WHOLE;
+	case '2': camMode = FOLLOWBALL;
+	}
 }
 
 void specialKeyboard(int key, int x, int y) {
@@ -30,6 +38,7 @@ int main(int argc, char** argv) {
 	glutCreateWindow("Pukimun Bullyball");
 	glutReshapeFunc(myReshape);
 	glutDisplayFunc(display);
+	glutKeyboardFunc(myKeyboard);
 	glutSpecialFunc(specialKeyboard);
 	glutTimerFunc(3, idlefunction, 0);
 	glutTimerFunc(17, idle2, 0);
